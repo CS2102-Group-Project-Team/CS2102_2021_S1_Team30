c8a6b42def97fbbb590a3ce22dda22a8aa7407e3
Joanna-YJA <54109347+Joanna-YJA@users.noreply.github.com>
2020-11-13 17:50:21 +0800

    Change config object

--- auth/init.js
+++ auth/init.js
@@ -11,7 +11,7 @@ const antiMiddleware = require('./antimiddle');
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  //ssl: true
+  ssl: true
 });
 
 function findUser (username, callback) {
